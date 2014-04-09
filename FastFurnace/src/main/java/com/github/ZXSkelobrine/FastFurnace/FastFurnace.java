b22b36d1ace8eb2478175124ecb0ce0f55012ad5
ZXSkelobrine <ZXSkelobrine@gmail.com>
2014-04-09 15:45:28 +0100

    Words.

--- FastFurnace/src/main/java/com/github/ZXSkelobrine/FastFurnace/FastFurnace.java
+++ FastFurnace/src/main/java/com/github/ZXSkelobrine/FastFurnace/FastFurnace.java
@@ -10,6 +10,7 @@ import net.milkbowl.vault.economy.Economy;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Furnace;
@@ -66,31 +67,34 @@ public class FastFurnace extends JavaPlugin {
 	@SuppressWarnings("deprecation")
 	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
 		if (command.getName().equalsIgnoreCase("fastfurnace") || command.getName().equalsIgnoreCase("ff")) {
-			getLogger().info("1");
 			if (sender instanceof Player) {
 				Player player = (Player) sender;
 				Block block = player.getTargetBlock(null, 20);
-				getLogger().info("2");
-				log.info(block.getType().name());
 				if (block.getType().equals(Material.FURNACE) || block.getType().equals(Material.BURNING_FURNACE)) {
 					if (player.hasMetadata("cooldown")) {
 						if (!(player.getMetadata("cooldown").get(0).asInt() > 0)) {
-							getLogger().info("3");
-							Furnace furnace = (Furnace) block.getState();
-							FurnaceInventory fi = furnace.getInventory();
-							ItemStack[] iss = fi.getContents();
-							List<ItemStack> contents = Arrays.asList(iss);
-							fi.setResult(new ItemStack(contents.get(OUTPUT).getType(), contents.get(OUTPUT).getAmount() + 8));
-							fi.setSmelting(new ItemStack(contents.get(COOKING).getType(), contents.get(COOKING).getAmount() - 8));
-							Material fuel = contents.get(BURNER).getType();
-							switch (fuel) {
-							case COAL:
-								fi.setFuel(new ItemStack(fuel, contents.get(BURNER).getAmount() - 1));
-								break;
+							Location bloc = block.getLocation();
+							bloc.setY(block.getY() + 1);
+							if (player.getWorld().getBlockAt(bloc).getType().equals(Material.IRON_BLOCK)) {
+								this.reloadConfig();
+								Furnace furnace = (Furnace) block.getState();
+								FurnaceInventory fi = furnace.getInventory();
+								ItemStack[] iss = fi.getContents();
+								List<ItemStack> contents = Arrays.asList(iss);
+								fi.setResult(new ItemStack(contents.get(OUTPUT).getType(), contents.get(OUTPUT).getAmount() + 8));
+								fi.setSmelting(new ItemStack(contents.get(COOKING).getType(), contents.get(COOKING).getAmount() - 8));
+								Material fuel = contents.get(BURNER).getType();
+								switch (fuel) {
+								case COAL:
+									fi.setFuel(new ItemStack(fuel, contents.get(BURNER).getAmount() - 1));
+									break;
+								}
+								FFEconomy.depleteMoney(player.getName(), 100D);
+								player.setMetadata("cooldown", new FixedMetadataValue(this, this.getConfig().getInt("cooldown")));
+								om("Done!", player);
+							} else {
+								om("Sorry, you must have an iron block on top of your furnace to do that!", player);
 							}
-							FFEconomy.depleteMoney(player.getName(), 100D);
-							player.setMetadata("cooldown", new FixedMetadataValue(this, this.getConfig().getInt("cooldown")));
-							om("Done!", player);
 						} else {
 							om("Sorry - you cant do that. Please wait " + player.getMetadata("cooldown").get(0).asInt() + " seconds before doing that command again.", player);
 						}
@@ -98,8 +102,12 @@ public class FastFurnace extends JavaPlugin {
 						player.setMetadata("cooldown", new FixedMetadataValue(this, 0));
 						om("Blast! Something went wrong. Please try the command again. Dont worry no money has been taken!", player);
 					}
-					return true;
+				} else {
+					om("You must be looking at a furnace to do that!", player);
 				}
+				return true;
+			} else {
+				log.info("Sorry you must be a player to do that");
 			}
 		}
 		return false;
